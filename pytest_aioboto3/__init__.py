190dc1d9a7fbc10348ab49223c1b8d435e5790d4
Oscar Caballero <oscar.caballero@bayes.gg>
2022-05-16 14:10:11 +0200

    Rename main package to pytest-aioboto3

---
+++ pytest_aioboto3/__init__.py
@@ -0,0 +1,4 @@
+from .aioboto3_fixtures import moto_patch_session, aioboto3_s3_client
+from .moto_fixtures import moto_services
+
+__all__ = ["moto_pathc_session", "aioboto3_s3_client", "moto_services"]
